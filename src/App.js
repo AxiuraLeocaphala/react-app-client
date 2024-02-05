1ecb6a4f15074697ccf4f093479bb24ad36c24bd
Domitory1 <jomunyts@gmail.com>
2024-02-05 15:13:46 +0300

    modified:   src/App.js modified:   src/components/button/button.jsx new file:   src/components/query/queryInsert renamed:    src/components/query/query.jsx -> src/components/query/querySelect.jsx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
-import Query from './components/query/query.jsx';
+import Query from './components/query/querySelect.jsx';
 import TruncateTextName from './components/trancateText/trancateTextName.jsx';
 import TruncateTextDescription from './components/trancateText/trancateTextDescription.jsx';
 import './App.css';
 
-const tg = window.Telegram.WebApp;
-
 function App() {
    const [renderedCards, setRenderedCards] = useState(false);
-
-   console.log(tg.initDataUnsafe.user.id);
 
    return (
       <div className="webApp">
--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import axios from 'axios';
 import './button.css';
 
+const tg = window.Telegram.WwbApp;
+
 const Button = ({price}) => {
 
     // Запрос серверу на добавление товара в корзину
     const handleClick = () => {
         axios.post('http://127.0.0.1:3001/data/addToBusket', {
-            price: price
+            price: price,
+            chatId : tg.initDataUnsafe.user.id
         })
         .then(responce => {
             // Обработка ответа от сервера
