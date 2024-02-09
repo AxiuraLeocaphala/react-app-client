5f2c9ea4cdc155b125f93c38ddd85e1891490030
Domitory1 <jomunyts@gmail.com>
2024-02-09 22:10:13 +0300

    modified:   src/components/query/queryAdd.jsx modified:   src/components/query/queryInsert.jsx modified:   src/components/query/queryReduce.jsx

--- src/components/query/queryAdd.jsx
+++ src/components/query/queryAdd.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const  QueryAdd = (chatId, productName) => {
-    return axios.post('http://127.0.0.1:3001/data/increaseQuantity', {
+    return axios.post('http://192.168.1.3:3001/data/increaseQuantity', {
         chatId,
         productName
     });
--- src/components/query/queryReduce.jsx
+++ src/components/query/queryReduce.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const  QueryReduce = (chatId, productName) => {
-    return axios.post('http://127.0.0.1:3001/data/reduceNumber', {
+    return axios.post('http://192.168.1.3:3001/data/reduceNumber', {
         chatId,
         productName
     });
