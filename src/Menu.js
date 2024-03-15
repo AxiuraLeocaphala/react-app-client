ad5e04b84037436e84d81a00722cb70b08decbcd
Domitory1 <jomunyts@gmail.com>
2024-03-15 16:13:32 +0300

    modified:   src/Menu.js modified:   src/components/hookTelegram/hookTelegram.jsx new file:   src/netlify.toml modified:   src/Menu.js

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
-import Preloader from './components/preloader/preloader.jsx';
+import React, {useState, useEffect} from 'react';
+import { useHistory } from 'react-router-dom';
+import { HookTelegram } from './components/hookTelegram/hookTelegram.jsx';
 import Main from './components/main/main.jsx';
 import Header from './components/header/header.jsx';
 import {QuerySelect} from './components/query/querySelect.jsx';
@@ -8,11 +9,35 @@ import './App.css';
 function Menu() {
     const [loadData, setLoadData] = useState(false);
     const [data, setData] = useState([]);
+    const {tg} = HookTelegram();
 
     const handleData = (data) => {
         setData(data);
         setLoadData(true);
     }
+
+    useEffect(() => {
+        tg.ready();
+        tg.MainButton.show();
+        if (typeof data[0] !== 'undefined') {
+            let totalPrice = 0; 
+            data[1].forEach(element => {
+                if (typeof element['Количество в корзине'] !== 'undefined'){
+                    totalPrice += element['Стоимость'];
+                }
+            });
+            if (totalPrice === 0){
+                tg.MainButton.text = 'Корзина';
+            } else {
+                tg.MainButton.setParams({
+                    text: `Корзина ${totalPrice}`
+                })
+            }
+        }
+        tg.MainButton.onClick(() => {
+            useHistory.push('/busket');
+        });
+    }, [data])
 
     return (
         <div className="WebApp">
--- src/components/hookTelegram/hookTelegram.jsx
+++ src/components/hookTelegram/hookTelegram.jsx
@@ -1,16 +1,18 @@
+import { useEffect } from "react";
+
 const tg = window.Telegram.WebApp;
 tg.expand();
 tg.MainButton.show();
-tg.MainButton.text = 'Корзина';
 
 let chatId;
 if (typeof tg.initDataUnsafe.id != "undefined") {
     chatId = tg.initDataUnsafe.id;
 } else {
-    chatId = 11111111;
+    chatId = 111111111;
 }
 
 export function HookTelegram () {
+
     return {
         tg,
         chatId
