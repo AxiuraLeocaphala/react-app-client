8b154be95bd89b92c42dfd9eb9bce155b9254c45
Domitory1 <jomunyts@gmail.com>
2024-06-07 12:48:39 +0300

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import axios from 'axios'
+import axios from 'axios'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Menu from './Menu';
 import Busket from './Busket';
 import ErrorPage from './components/ErrorPage/errorPage';
-//import { useTelegram } from './components/hooks/useTelegram';
+import { useTelegram } from './components/hooks/useTelegram';
 
-/*
 async function Auth() {
     const { tg } = useTelegram.getTelegramData();
     const initData = tg.initData;
@@ -19,18 +18,18 @@ async function Auth() {
         throw new Error(error, {status: error.reponse.status});
     })
 }
-*/
+
 const router = createBrowserRouter([
     {   
         path: "/",
         element: <Menu/>,
         errorElement: <ErrorPage/>,
-        /*
+        
         loader: async () => {
             const data = await Auth();
             return data;
         }
-        */
+        
     },
     {
         path: "/busket",
