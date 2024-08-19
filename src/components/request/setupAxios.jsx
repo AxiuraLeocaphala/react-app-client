2779ed0cea58e9512dac201042a54179ee37d117
Domitory1 <jomunyts@gmail.com>
2024-08-19 13:26:32 +0300

    modified:   src/components/request/authWrapper.js modified:   src/components/request/setupAxios.jsx

--- src/components/request/setupAxios.jsx
+++ src/components/request/setupAxios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getCookie } from './cookie';
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:3002',
+    baseURL: 'product-api',
 });
 
 instance.interceptors.request.use(config => {
