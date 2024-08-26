153deddce12fcec3ebfe092735ffd8a3fed0f1a8
Domitory1 <jomunyts@gmail.com>
2024-08-26 15:25:47 +0300

    modified:   src/components/request/authWrapper.js modified:   src/components/request/loaderMenu.jsx modified:   src/components/request/setupAxios.jsx

--- src/components/request/loaderMenu.jsx
+++ src/components/request/loaderMenu.jsx
@@ -2,7 +2,7 @@ import QualifierErrors from "./_qualifierErrors";
 import instance from "./setupAxios.jsx";
 
 async function LoaderMenu() {
-    return instance.get(`/data/price-list`)
+    return instance.get(`/data/priceList`)
     .then(response => response)
     .catch(error => QualifierErrors(error));
 } 
