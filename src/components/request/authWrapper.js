090b40fc6a180534544dab156efb1ba9d40919e9
Domitory1 <jomunyts@gmail.com>
2024-08-27 13:58:13 +0300

    modified:   src/components/request/authWrapper.js modified:   src/components/request/setupAxios.jsx

--- src/components/request/authWrapper.js
+++ src/components/request/authWrapper.js
@@ -7,18 +7,15 @@ export async function AuthWrapper() {
     const {tg} = useTelegram.getTelegramData();
     const initData = tg.initData;
     
-    await axios.post('http://127.0.0.1:3003/auth/checkInitData', {
-        initData: initData
+    await axios.post('user-api/auth/checkInitData', {
+        initData: "query_id=AAGqQNUmAAAAAKpA1SaKm8S3&user=%7B%22id%22%3A651509930%2C%22first_name%22%3A%22%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22domittory8%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1724059856&hash=968f47bc7035b8f9630aa5de06e818f47f45f9dfd079a2749f2a61dc6b168c5a"
     })
     .then(res => {
         setCookie('accessToken', res.data.accessToken, {'max-age': 60});
         setCookie('refreshToken', res.data.refreshToken, {'max-age': 120});
         ScheduleRefreshTokens();
     })
-    .catch(err => {
-        console.log(err);
-        QualifierErrors(err);
-    });
+    .catch(err => QualifierErrors(err));
 }
 
 export function ScheduleRefreshTokens() {
@@ -34,7 +31,7 @@ export function ScheduleRefreshTokens() {
 export async function RefreshTokens() {
     const refreshToken = getCookie('refreshToken');
 
-    axios.post('http://127.0.0.1:3003/auth/refreshTokens', {
+    axios.post('user-api/auth/refreshTokens', {
         refreshToken: refreshToken
     })
     .then(res => {
