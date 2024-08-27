cc28a6377c0e091f226fc2053c7b2ea87478d8c5
Domitory1 <jomunyts@gmail.com>
2024-08-27 15:04:47 +0300

    modified:   src/components/request/authWrapper.js

--- src/components/request/authWrapper.js
+++ src/components/request/authWrapper.js
@@ -11,6 +11,7 @@ export async function AuthWrapper() {
         initData: initData
     })
     .then(res => {
+        console.log(res)
         setCookie('accessToken', res.data.accessToken, {'max-age': 60});
         setCookie('refreshToken', res.data.refreshToken, {'max-age': 120});
         ScheduleRefreshTokens();
@@ -21,7 +22,6 @@ export async function AuthWrapper() {
 export function ScheduleRefreshTokens() {
     console.log('schedule');
     const accessToken = getCookie('accessToken');
-    console.log(accessToken)
     const exp = JSON.parse(atob(accessToken.split('.')[1])).exp;
     const timeout = (exp - Math.round(Date.now() / 1000)) * 1000 - 30000;
 
