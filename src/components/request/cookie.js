e8ee6e2f50cb2858e254dcb3056209815121f9ae
Domitory1 <jomunyts@gmail.com>
2024-08-28 13:04:09 +0300

    modified:   src/components/request/cookie.js

--- src/components/request/cookie.js
+++ src/components/request/cookie.js
@@ -2,6 +2,7 @@ export function getCookie(name) {
     let matches = document.cookie.match(new RegExp(
         "(?:^|; )" + name.replace(/([.$*|{}()\\[\]+^])/g, '\\$1') + "=([^;]*)"
     ))
+    console.log(matches);
     console.log(name, decodeURIComponent(matches[1]))
     return matches ? decodeURIComponent(matches[1]) : undefined;
 }
